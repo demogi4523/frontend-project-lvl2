fdf81d5857fcf6e6648720dc9e20cb4ae813f790
demogi4523 <demogi4523@yandex.ru>
2021-03-26 05:50:31 +0800

    initial commit

---
+++ bin/gendiff.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import program from '../src/index.js';
+
+program.parse();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { program } from 'commander';
+
+program.version('0.0.1');
+program.description('Compares two configuration files and shows a difference.');
+
+export default program;
