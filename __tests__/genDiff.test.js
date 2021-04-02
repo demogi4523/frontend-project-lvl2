59d4ed44c336dbac5ee453a342972e1283115d5e
demogi4523 <demogi4523@yandex.ru>
2021-04-03 00:05:43 +0800

    quick fix effort#16 to add code coverage in CI

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,42 +5,6 @@ import genDiff from '../src/genDiff.js';
 
 const __dirname = dirname('');
 
-const a = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
-
-const b = {
-  timeout: 20,
-  verbose: true,
-  host: 'hexlet.io',
-};
-
-const filepaths = [
-  {
-    filepath: 'a.json',
-    data: a,
-  },
-  {
-    filepath: 'b.json',
-    data: b,
-  },
-];
-
-beforeAll(() => {
-  filepaths.forEach(({ filepath: fp, data }) => {
-    fs.writeFileSync(path.join(__dirname, '__tests__', '__fixtures__', fp), JSON.stringify(data));
-  });
-});
-
-afterAll(() => {
-  filepaths.forEach(({ filepath: fp }) => {
-    fs.unlinkSync(path.join(__dirname, '__tests__', '__fixtures__', fp));
-  });
-});
-
 const expectedDiff = [
   '- follow: false',
   '  host: hexlet.io',
@@ -50,7 +14,9 @@ const expectedDiff = [
   '+ verbose: true',
 ].toString();
 
+const filepaths = ['a.json', 'b.json'];
+
 test('Test gendiff for two json files', () => {
-  const [fp1, fp2] = filepaths.map((item) => path.join('__tests__', '__fixtures__', item.filepath));
+  const [fp1, fp2] = filepaths.map((fp) => path.join('__tests__', '__fixtures__', fp));
   expect(genDiff(fp1, fp2).toString()).toBe(expectedDiff);
 });
